5f67e66fc3b180a0945f00e930288f19a463880e
Fabian Suarez <fsuarez120@unab.edu.co>
2021-12-03 11:33:22 -0500

    ajuste a la modificacion del dia sin iva

--- app/Views/contents/shoppingcart.php
+++ app/Views/contents/shoppingcart.php
@@ -6,7 +6,7 @@
         </div>
         <div class="row">
             <div class="col-lg-7">
-                <img src="<?= base_url(). '/public/pictures/peradk/dia_sin_iva_2.jpeg' ?>" class="img-fluid" alt="fdsfa">
+                <img src="<?= base_url(). '/public/pictures/peradk/dia_sin_iva_03-12.jpeg' ?>" class="img-fluid" alt="fdsfa">
                 <?php
 
                 if (count($cart) <= 0) {
