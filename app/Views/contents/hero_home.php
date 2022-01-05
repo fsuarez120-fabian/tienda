e95090e57f0fbed231124c97f73a67fc9129c70e
Fabian Suarez <fsuarez120@unab.edu.co>
2022-01-05 14:00:10 -0500

    Se quito lo del sorteo de la moto

--- app/Views/contents/hero_home.php
+++ app/Views/contents/hero_home.php
@@ -1,24 +1,33 @@
-<section id="hero" class="d-flex align-items-center">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
-                <div class="text-center">
-                    <h1>BRUTOS PERO DECIDIDOS!!!</h1>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <iframe class="video-mayorista" src="https://www.youtube.com/embed/DVhFnXpvM9M" title="PeRa DK" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div>
-                </div>
-
-                <div class="text-center">
-                    <a href="#about" class="btn-get-started scrollto">PeRa Productos</a>
-                    <a data-href="https://www.youtube.com/watch?v=BIrhmzL_bdA" class="venobox btn-watch-video" data-autoplay="true" data-vbtype="video">Nuestra Historia <i class="icofont-play-alt-2"></i></a>
-                </div>
-            </div>
-            <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
-                <img src="<?= base_url() ?>/public/pictures/peradk/twologo3.png" class="img-fluid animated" alt="">
-            </div>
-        </div>
-    </div>
+<section id="hero" class="d-flex align-items-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+                <div class="text-center">
+                    <img src="<?= base_url('/public/pictures/peradk/creo_en_mi_me_lo_merezco_blaco.png') ?>" class="img-fluid animated" alt="">
+                    <!-- <h2 style="margin-bottom: 1rem; font-size: 20px; color: #fff;">G&aacute;nate una hermosa P E R A Moto, para que puedas llevar felicidad a cada uno de los hogares colombianos &#127824;&#128156;</h2> -->
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <br><br>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="text-center">
+                                    <a href="#about" class="btn-get-started scrollto">P E R A PRODUCTOS</a>
+                                </div>
+                            </div>
+                            <!-- <div class="col-md-12">
+                                <div class="text-center">
+                                    <a href="https://www.youtube.com/watch?v=0wwONOCrWiw" class="venobox btn-watch-video" data-vbtype="video" data-autoplay="true">¿C&oacute;mo participar? <i class="icofont-play-alt-2"></i></a>
+                                </div>
+                            </div> -->
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <!-- <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+                <img src="<?= base_url() ?>/public/pictures/peradk/logo_morado.png" class="img-fluid" alt="">
+            </div> -->
+        </div>
+    </div>
 </section>
