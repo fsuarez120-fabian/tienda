970042cc54dcc851ef430f24eaf6de1380235495
Fabian Suarez <fsuarez120@unab.edu.co>
2022-01-19 14:05:35 -0500

    nueva modificacion para el titulos del formulario de distribuidor autorizado

--- app/Views/deaders/form.php
+++ app/Views/deaders/form.php
@@ -68,7 +68,9 @@
                 <div class="row justify-content-center">
                     <div class="col-lg-8 text-center mx-auto my-auto">
                         <?php if (empty(session()->get('form_completed'))) : ?>
-                            <h1 style="font-family: 'Bebas';background-color: #D290F4; border-radius: 10px; padding: 1rem; color: #000000; font-weight: 600;">¿Quieres ser PERA distribuidor autorizado?</h1>
+                            <h1 style="font-family: 'Bebas';background-color: #D290F4; border-radius: 10px; padding: 1rem; color: #000000; font-weight: 600;">
+                                ¿Quieres ser <br> PERA distribuidor <br> autorizado?
+                            </h1>
                             <p class="lead mb-4 text-white opacity-8 mt-5">Déjanos algunos datos para poderte contactar.</p>
                             <!-- <button type="submit" class="btn bg-white text-dark">Create Account</button> -->
                         <?php else : ?>
