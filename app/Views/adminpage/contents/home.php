1e44d56127ddfb8af8ea7f8ea408c2ab3576e12b
Fabian Suarez <fsuarez120@unab.edu.co>
2022-01-22 08:59:59 -0500

    se quito el logo que estaba en el home del admin page

--- app/Views/adminpage/contents/home.php
+++ app/Views/adminpage/contents/home.php
@@ -1,41 +1,36 @@
-<!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>BRUTOS PERO DECIDIDOS!!!</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item active">Home</li>
-                    </ol>
-                </div>
-            </div>
-        </div><!-- /.container-fluid -->
-    </section>
-
-    <section class="content">
-        <div class="container-fluid">
-            <!-- ======= Hero Section ======= -->
-            <?php
-            if (session()->idadministrator == '1094280366') {
-            ?>
-
-            <h2>Eliana digita el numero que me dijiste ayer, recuerdas?</h2>
-            <form action="<?=base_url().route_to('admin_page_msg_encripted');?>" method="post">
-                <input type="password" name="pass">
-                <button type="submit">Seguir</button>
-            </form>
-            <?php
-            }
-            ?>
-            <div class="text-center">
-                <img src="<?= base_url() ?>/public/pictures/peradk/twologo1.png" class="img-fluid" alt="">
-            </div>
-
-        </div>
-
-    </section><!-- End Hero -->
+<div class="content-wrapper">
+    <!-- <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>BRUTOS PERO DECIDIDOS!!!</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item active">Home</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </section> -->
+
+    <section class="content">
+        <div class="container-fluid">
+            <!-- ======= Hero Section ======= -->
+            <?php
+            if (session()->idadministrator == '1094280366') {
+            ?>
+                <h2>Eliana digita el numero que me dijiste ayer, recuerdas?</h2>
+                <form action="<?= base_url() . route_to('admin_page_msg_encripted'); ?>" method="post">
+                    <input type="password" name="pass">
+                    <button type="submit">Seguir</button>
+                </form>
+            <?php
+            }
+            ?>
+            <!-- <div class="text-center">
+                <img src="<?= base_url() ?>/public/pictures/peradk/twologo1.png" class="img-fluid" alt="">
+            </div> -->
+        </div>
+    </section><!-- End Hero -->
 </div>
