6f9f55314a99695b55aca8ea6b2b5c637ad273d1
fsuarez120-fabian <fsuarez120@unab.edu.co>
2022-01-28 10:55:26 -0500

    Cambios desde el hosting, para cambiar a nuevo dominio peracolombia y crear las redirecciones

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'https://www.tienda.peradk.com/';
+	public $baseURL = 'https://peracolombia.com/';
 
 	/*
 	|--------------------------------------------------------------------------
